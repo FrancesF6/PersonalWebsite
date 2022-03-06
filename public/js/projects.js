557a4036bae80c4287948e6f62876e8b071b08f7
Frances Fang <francesfang@cmail.carleton.ca>
2022-03-06 16:07:05 -0500

    modify navbar and project page's structures

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,3 +1,4 @@
+// project modal section
 let modal = document.getElementById("demo-modal");
 let modalPic = document.getElementById("modal-pic");
 let modalCaption = document.getElementById("modal-caption");
@@ -6,7 +7,7 @@ let modalChoose = document.getElementById("modal-prev-next");
 function displayModal(prjID, picID) {
     modal.style.display = "block";
     
-    let demo = document.getElementById(`demo-${prjID}`);
+    let demo = document.getElementById(`${prjID}-demo`);
     let numPics = demo.childNodes.length;
     let demoPic = demo.childNodes[picID];
     modalPic.src = demoPic.src;
@@ -24,4 +25,27 @@ function closeModal() {
     modalPic.removeAttribute("src");
     modalCaption.innerHTML = '';
     modalChoose.innerHTML = '';
+}
+
+
+// expand and collapse descriptions and details
+function expandCollapse(elem) {
+    let delim = elem.id.indexOf('-');
+    let section = elem.id.slice(0, delim);
+    let prjID = elem.id.slice(delim+1);
+    let block = document.getElementById(`${prjID}-${section}`);
+
+    if (elem.className == "expand") {
+        elem.className = "collapse";
+        block.style.display = "block";
+        if (section == "des") elem.innerHTML = "Description &#9662;";
+        else if (section == "det") elem.innerHTML = "Details &#9662;";
+        else if (section == "demo") elem.innerHTML = "Screenshots &#9662;";
+    } else if (elem.className == "collapse") {
+        elem.className = "expand";
+        block.style.display = "none";
+        if (section == "des") elem.innerHTML = "Description &#9656;";
+        else if (section == "det") elem.innerHTML = "Details &#9656;";
+        else if (section == "demo") elem.innerHTML = "Screenshots &#9656;";
+    }
 }
